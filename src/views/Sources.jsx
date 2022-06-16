33f3a64982a6443e9e98166051c10d594479c341
Tapir <helloworld.lkl@gmail.com>
2022-06-16 15:36:42 +0800

    Updated Sources page

--- src/views/Sources.jsx
+++ src/views/Sources.jsx
@@ -1,11 +1,19 @@
 export default function Sources() {
+  function hiMom() {
+    let a = Math.random();
+    debugger
+    let b = 42;
+    let c = a + b;
+    console.log('Hi, mom!');
+    console.log(c);
+  }
   return (
     <div>
       <h1>Sources</h1>
-
+      <button onClick={hiMom}>Click Me!</button>
       <ol>
         <li>Code folding.</li>
-        <li>Add logger.</li>
+        <li>Add logger. </li>
         <li>Black box script.</li>
       </ol>
     </div>
